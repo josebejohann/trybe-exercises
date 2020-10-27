7968851a3fb401349a11e810949a2eef5be83b1e
josebejohann <josebernardojohann@gmail.com>
2020-10-27 14:47:35 -0300

    Improves code

--- bloco-4/dia-3/blank-base-pyramid.script.js
+++ bloco-4/dia-3/blank-base-pyramid.script.js
@@ -4,14 +4,14 @@ for (j = 1; j <= Math.ceil(n / 2); j += 1) {
   blankBasePyramid = '';
 
   if (j < Math.ceil(n / 2)) {
-    for (let k = 1; k <= Math.ceil(n / 2 - j); k += 1) {
+    for (let k = 1; k <= Math.ceil(n / 2 - j); k++) {
       blankBasePyramid += ' ';
     }
 
     if (n % 2 == 0) {
       blankBasePyramid += '*';
 
-      for (let w = 2; w < j * 2; w += 1) {
+      for (let w = 2; w < j * 2; w++) {
         blankBasePyramid += ' ';
       }
 
@@ -22,7 +22,7 @@ for (j = 1; j <= Math.ceil(n / 2); j += 1) {
       } else {
         blankBasePyramid += '*';
 
-        for (let w = 2; w < j * 2 - 1; w += 1) {
+        for (let w = 2; w < j * 2 - 1; w++) {
           blankBasePyramid += ' ';
         }
 
@@ -30,7 +30,7 @@ for (j = 1; j <= Math.ceil(n / 2); j += 1) {
       }
     }
 
-    for (w = 1; w <= Math.ceil(n / 2 - j); w += 1) {
+    for (w = 1; w <= Math.ceil(n / 2 - j); w++) {
       blankBasePyramid += ' ';
     }
 
