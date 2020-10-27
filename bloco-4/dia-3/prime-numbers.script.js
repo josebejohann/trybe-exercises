0a3a331b76d9d28684a4c1063f328a2dd14b2c75
josebejohann <josebernardojohann@gmail.com>
2020-10-27 15:07:21 -0300

    Excludes negative numbers

--- bloco-4/dia-3/prime-numbers.script.js
+++ bloco-4/dia-3/prime-numbers.script.js
@@ -1,14 +1,18 @@
-let n = 7;
+let n = 0;
 let d = 0;
 
-for (j = n; j > 0; j--) {
-  if (n % j == 0) {
-    d++;
+if (n >= 0) {
+  for (j = n; j > 0; j--) {
+    if (n % j == 0) {
+      d++;
+    }
   }
-}
 
-if (d == 2) {
-  console.log(`${n} IS a prime number.`);
+  if (d == 2) {
+    console.log(`${n} IS a prime number.`);
+  } else {
+    console.log(`${n} is NOT a prime number.`);
+  }
 } else {
-  console.log(`${n} is NOT a prime number.`);
+  console.log('There is no prime negative number.');
 }
