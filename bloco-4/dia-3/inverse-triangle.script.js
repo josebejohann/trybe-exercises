76a7c5bb4eb1e1687dc7d781cb1df43ff69d24d1
josebejohann <josebernardojohann@gmail.com>
2020-10-27 14:42:04 -0300

    Creates an inverse triangle using a for loop

---
+++ bloco-4/dia-3/inverse-triangle.script.js
@@ -0,0 +1,22 @@
+let n = 5;
+let inverseTriangle = '';
+
+if (n > 1) {
+  for (i = 1; i <= n; i++) {
+    inverseTriangle = '';
+
+    for (j = n; j > i; j--) {
+      inverseTriangle += ' ';
+    }
+
+    for (k = 0; k < i; k++) {
+      inverseTriangle += '*';
+    }
+
+    console.log(inverseTriangle);
+  }
+} else {
+  console.log(
+    'We cannot pass negative values (or zeros) to the side of a triangle.'
+  );
+}
