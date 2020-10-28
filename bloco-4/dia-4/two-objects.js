3b96364601330be0df58423004ec6694cbcb0f3b
josebejohann <josebernardojohann@gmail.com>
2020-10-28 16:28:09 -0300

    Shows both objects' values

---
+++ bloco-4/dia-4/two-objects.js
@@ -0,0 +1,21 @@
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: 'Sim',
+};
+
+let anotherInfo = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: 'Sim',
+};
+
+for (i in info) {
+  if (i !== 'recorrente') {
+    console.log(`${info[i]} e ${anotherInfo[i]}`);
+  } else {
+    console.log('Ambos reocorrentes');
+  }
+}
